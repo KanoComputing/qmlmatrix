84cac685cfd06b6de9fc264ddd163ae4dace7b91
Albert Casals <skarbat@gmail.com>
2018-06-20 14:35:17 +0000

    Typo in instructions to run the tests

--- autotest/test_ssaver_loop.sh
+++ autotest/test_ssaver_loop.sh
@@ -13,7 +13,7 @@
 #  2) edit /usr/share/kano-desktop/kdesk/.kdeskrc, set ScreenSaverTimeout: 15
 #  3) Make sure you are on the Dashboard on idle status, then run:
 #
-#  $./ssaver-loop.sh > $HOME/ssaver-loop.log 2>&1
+#  $./test_ssaver_loop.sh > $HOME/ssaver-loop.log 2>&1
 #
 #  Recommend to use a screen multiplexer.
 #  A GPU memory sample is saved on each transition. Grep for "reloc" on the logfile.
