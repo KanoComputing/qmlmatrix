da7244d61d63b17d444f52aa8a39e66bc6e4de2a
Albert Casals <skarbat@gmail.com>
2018-06-20 14:41:18 +0000

    Adding a tool to generate GPU memory graphs

---
+++ autotest/plot_gpu_mem.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+# plot_gpu_mem.py <ssaver_tests_logfile>
+#
+# This script will take a plain logfile from the Screen Saver tests,
+# and generate a plot graph of the GPU free memory samples throughout the tests.
+#
+# Y axis shows the amount of free GPU memory, X axis shows the number of samples.
+#
+
+import sys
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+
+    leak_numbers=[]
+    output_graph_file='plot_gpu_mem.png'
+
+    if len(sys.argv) < 2:
+        print 'please provide a Screen Saver tests log file'
+        sys.exit(1)
+    else:
+        logfile=sys.argv[1]
+
+    leak_lines=open(logfile, 'r').readlines()
+
+    for l in leak_lines:
+        if l.startswith('reloc'):
+            a_leak=l.split('=')[1].strip('M\n')
+            leak_numbers.append(a_leak)
+
+    print leak_numbers
+
+    x = range(0, len(leak_numbers))
+    plt.plot(x, leak_numbers)
+    plt.savefig(output_graph_file)
